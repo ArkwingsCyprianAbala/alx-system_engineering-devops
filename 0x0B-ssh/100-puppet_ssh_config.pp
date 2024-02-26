21c19ea892b391127bf3247659d23b1540e3af97
Arkwings Cyprian Abala <arkwingscyprianabala@gmail.com>
2024-02-25 18:43:24 -0800

    Using puppet for automation

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# using puppet to make changes to our configuration file
+
+file { 'etc/ssh/ssh_config':
+	ensure => present,
+
+content => "
+
+	#SSH client configuration
+	Host*
+	IdentityFile ~/.ssh/school
+	PasswordAuthentication no
+	",
+}
